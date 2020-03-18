81fc3aa12d4b28178099d186e85f0175d4a6d1ca
Berkay Sebat <bsebat@kwi.com>
2020-03-18 11:35:31 -0400

    second commit

---
+++ MarvelDemo/MarvelViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  MarvelDemo
+//
+//  Created by Berkay Sebat on 3/17/20.
+//  Copyright © 2020 marvel. All rights reserved.
+//
+
+import UIKit
+
+class MarvelViewController: UIViewController {
+    @IBOutlet var coverImage: UIImageView!
+    @IBOutlet var titleLabel: UILabel!
+    @IBOutlet var descBox: UITextView!
+
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+}
+
